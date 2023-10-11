3e79277a4a92e51b7cb9714f7dad4c40f9b9daef
HuangZhe885 <634143057@qq.com>
2023-10-11 17:16:44 +0800

    first commit

---
+++ build/temp.linux-x86_64-cpython-37/src/cuhash/CMakeFiles/cuhash.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../../../lib.linux-x86_64-cpython-37/spconv/libcuhash.pdb"
+  "../../../lib.linux-x86_64-cpython-37/spconv/libcuhash.so"
+  "CMakeFiles/cuhash.dir/cmake_device_link.o"
+  "CMakeFiles/cuhash.dir/hash_functions.cpp.o"
+  "CMakeFiles/cuhash.dir/hash_functions.cpp.o.d"
+  "CMakeFiles/cuhash.dir/hash_functions.cu.o"
+  "CMakeFiles/cuhash.dir/hash_functions.cu.o.d"
+  "CMakeFiles/cuhash.dir/hash_table.cpp.o"
+  "CMakeFiles/cuhash.dir/hash_table.cpp.o.d"
+  "CMakeFiles/cuhash.dir/hash_table.cu.o"
+  "CMakeFiles/cuhash.dir/hash_table.cu.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/cuhash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
